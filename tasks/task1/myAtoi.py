c10abd5fcdcdb15afecf8e3a582ea5d42a669b47
IvanMijic0 <ivanmijic159951@gmail.com>
2023-07-19 21:29:30 +0200

    Bolje rješenje

--- tasks/task1/myAtoi.py
+++ tasks/task1/myAtoi.py
@@ -1,24 +1,17 @@
+import re
+
+
 def myAtoi(s: str) -> int:
     s = s.strip()
 
-    if not s:
+    match = re.match(r'^[+-]?\d+', s)
+
+    if match:
+        num_str = match.group()
+        result = int(num_str)
+
+        result = max(min(result, 2 ** 31 - 1), -2 ** 31)
+
+        return result
+    else:
         return 0
-
-    sign = 1
-    if s[0] == '-':
-        sign = -1
-        s = s[1:]
-    elif s[0] == '+':
-        s = s[1:]
-
-    num = 0
-    for char in s:
-        if not char.isdigit():
-            break
-        num = num * 10 + int(char)
-
-    num *= sign
-
-    num = max(min(num, 2**31 - 1), -2**31)
-
-    return num
